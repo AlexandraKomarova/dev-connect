f0b385715bfe3c535a2572d5fb7220e2f91a0d2b
Sonechka M <s212288s@gmail.com>
2020-04-02 10:26:19 -0400

    login form

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import axios from "axios"
+import { Link } from "react-router-dom"
 
 export const Register = () => {
   const [formData, setFormData] = useState({
@@ -18,24 +18,7 @@ export const Register = () => {
     if(password !== password2) {
       console.log("Passwords do not match" )
     } else {
-      const newUser = {
-        name,
-        email,
-        password
-      }
-
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-        const body = JSON.stringify(newUser)
-        const res = await axios.post("/api/users", body, config)
-        console.log(res.data)
-      } catch (err) {
-        console.error(err.response.data)
-      }
+      console.log("success")
     }
   }
 
@@ -61,8 +44,7 @@ export const Register = () => {
         required/>
         <small className="form-text"
           >This site uses Gravatar so if you want a profile image, use a
-          Gravatar email</small
-        >
+          Gravatar email</small>
       </div>
       <div className="form-group">
         <input
@@ -87,7 +69,7 @@ export const Register = () => {
       <input type="submit" className="btn btn-primary" value="Register" />
     </form>
     <p className="my-1">
-      Already have an account? <a href="login.html">Sign In</a>
+      Already have an account? <Link to="/login">Sign In</Link>
     </p>
   </>
 }
