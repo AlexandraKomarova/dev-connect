abde25bc01a504ebac0053cb40b799ff07658eb7
Sonechka M <s212288s@gmail.com>
2020-04-07 21:58:26 -0400

    ProfileTop and ProfileAbout component

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 import Spinner from "../layout/Spinner"
+import ProfileTop from "./ProfileTop"
+import ProfileAbout from "./ProfileAbout"
 import { getProfileById } from "../../actions/profile"
 
 // you can get id that is in the url by saying props.match 
@@ -10,21 +12,33 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
   useEffect(() => {
     // this should match the id in the url
     getProfileById(match.params.id)
-  }, [getProfileById]) // run immidiately when profile mounts
-  return <>
-      {profile === null || loading ? <Spinner /> :
-      <>
-        <Link to="/profiles" className="btn btn-light">
-          Back to Profiles
-        </Link>
-        {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
-        <Link to="/edit-profile" className="btn btn-dark">
-          Edit Profile
-        </Link>)}
-      </>}
+  }, [getProfileById, match.params.id]) // run immidiately when profile mounts
+  return (
+    <>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to='/profiles' className='btn btn-light'>
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+          <div class='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
+        </>
+      )}
     </>
-  
+  );
 }
+
 Profile.propTypes = {
   getProfileById: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
